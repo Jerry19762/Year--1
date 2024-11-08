9d44147eb2e0d4298503f0033673313cf2c88a9d
Jerry19762 <aafaqhussain008@gmail.com>
2024-11-08 14:16:54 +0000

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,12 @@
+num1=int(input("First number"))
+num2=int(input("Second number"))
+
+sum_result= num1 + num2
+difference_result= num1 - num2
+product_result= num1 * num2
+quotient_result= num1 / num2
+
+print(sum_result)
+print(difference_result)
+print(product_result)
+print(quotient_result)
---
+++ grades.py
@@ -0,0 +1,14 @@
+student_mark= int(input("input student marks"))
+if student_mark >=70:
+    print("grade: A")
+elif student_mark >=60:
+    print("grade: B")
+elif student_mark >=50:
+    print("grade: C")
+elif student_mark >=40:
+    print("grade: D")
+elif student_mark >=30:
+    print("grade: E")
+elif student_mark >=20:
+    print("grade: F")
+            
