369d7b959ae27b7c882bce8e3b5135ca3f3e0ccc
Jerry19762 <aafaqhussain008@gmail.com>
2024-11-22 14:07:49 +0000

    Add files via upload

---
+++ listops.py
@@ -0,0 +1,11 @@
+my_list=(1,2,9,3,5,7,6,4,8,32)
+adding = sum(my_list)
+largest_element = max(my_list)
+smallest_element = min(my_list)
+
+print("List:",my_list)
+print("Largest element in the list:",largest_element)
+print("Smallest element in the list:",smallest_element)
+print("Sum of the list:", adding)
+print("revrse order of the list:", my_list[::-1])
+
