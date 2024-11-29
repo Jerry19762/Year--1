06b47dc651803dbbcdeabf1a334055c352d5333f
Jerry19762 <aafaqhussain008@gmail.com>
2024-11-29 14:08:06 +0000

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,14 @@
+def extract_and_rearrange(string):
+    str_1 = "".join(reversed(string[0:4].split('g'))).capitalize()
+    str_2 = "".join(string[6:13].split('ro'))
+    str_3 = "".join(reversed(list(string[14:20]))).split(string[17])
+    str_4 = "".join(string[21:29].split(string[23:28]))
+
+    return str_1 + " " + str_2 + " " + ''.join(str_3) + " " + str_4
+
+def ultra_extract_and_rearrange(string):
+    first_transform = extract_and_rearrange(string)
+    return first_transform
+
+print(ultra_extract_and_rearrange("egthb quirock nwoGrb forijmpxv"))
+
